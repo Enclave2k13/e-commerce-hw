1ca61647d5db988a5ad9c5cd35e4212d3caef9e3
Nikita Komissarov <enclave13@yandex.ru>
2025-11-19 14:21:09 +0300

    add inheritance

---
+++ src/lawn_grass.py
@@ -0,0 +1,14 @@
+"""Модуль газонной травы."""
+
+from product import Product
+
+
+class LawnGrass(Product):
+    """Представляет газонною траву в магазине."""
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        """Инициализация газонной травы."""
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
---
+++ src/smartphone.py
@@ -0,0 +1,15 @@
+"""Модуль смартфонов."""
+
+from product import Product
+
+
+class Smartphone(Product):
+    """Представляет смартфоны в магазине."""
+
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        """Инициализация смартфона."""
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
